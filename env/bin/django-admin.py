ae5907163cc4019b4c9347cf638000fd5e9c9d5d
jiangwei <diablos327@aliyun.com>
2016-03-02 05:25:31 -0500

    added virtualenv sample.

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/wenshang/git/my_first_restful/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/wenshang/git/my_first_restful/virtualenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
